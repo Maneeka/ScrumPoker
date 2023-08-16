478546a5a111f34a5ff7e67739723cb6ea5d901f
mmishra1 <mmishra1@ebsco.com>
2023-08-16 11:04:38 -0400

    refactor: moved point buttons to separate component

---
+++ client/scrum-poker/src/components/Points.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+
+export function Points({socket}){
+    const [activeButton, setActiveButton] = useState(null);
+
+    const handleButtonClick = (buttonId) => {
+        if (activeButton === buttonId) {
+          setActiveButton(null); // Toggle off the active button
+        } else {
+          setActiveButton(buttonId); // Set the clicked button as active
+        }
+    };
+
+    return (
+        <>
+            <Button variant={activeButton === 0 ? 'contained' : 'outlined'} color={activeButton === 0 ? 'secondary' : 'primary'} onClick={() => handleButtonClick(0)} > 0 </Button>
+            <Button variant={activeButton === 0.5 ? 'contained' : 'outlined'} color={activeButton === 0.5 ? 'secondary' : 'primary'} onClick={() => handleButtonClick(0.5)} > 0.5 </Button>
+            <Button variant={activeButton === 1 ? 'contained' : 'outlined'} color={activeButton === 1 ? 'secondary' : 'primary'} onClick={() => handleButtonClick(1)} >1 </Button>
+            <Button variant={activeButton === 2 ? 'contained' : 'outlined'} color={activeButton === 2 ? 'secondary' : 'primary'} onClick={() => handleButtonClick(2)} >2 </Button>
+            <Button variant={activeButton === 3 ? 'contained' : 'outlined'} color={activeButton === 3 ? 'secondary' : 'primary'} onClick={() => handleButtonClick(3)} >3 </Button>
+            <Button variant={activeButton === 5 ? 'contained' : 'outlined'} color={activeButton === 5 ? 'secondary' : 'primary'} onClick={() => handleButtonClick(5)} >5 </Button>
+            <Button variant={activeButton === 8 ? 'contained' : 'outlined'} color={activeButton === 8 ? 'secondary' : 'primary'} onClick={() => handleButtonClick(8)} >8 </Button>
+            <Button variant={activeButton === 13 ? 'contained' : 'outlined'} color={activeButton === 13 ? 'secondary' : 'primary'} onClick={() => handleButtonClick(13)} >13 </Button>
+                    
+        </>
+    );
+}
--- client/scrum-poker/src/components/Room.js
+++ client/scrum-poker/src/components/Room.js
@@ -1,25 +1,17 @@
 import { useParams } from "react-router-dom";
 import React, { useEffect, useState } from 'react';
-import Button from '@mui/material/Button';
+import { Points } from "./Points";
 
 export function Room({socket}) {
     const { id } = useParams()
     const [members, setMembers] = useState({}); //object containing all the {displayName: member obj} pairs
-    const [activeButton, setActiveButton] = useState(null);
+    
 
     useEffect(() => {
         socket.on('updateMembers', (updatedMembers) => {
             setMembers(updatedMembers);
         })
     })
-
-    const handleButtonClick = (buttonId) => {
-        if (activeButton === buttonId) {
-          setActiveButton(null); // Toggle off the active button
-        } else {
-          setActiveButton(buttonId); // Set the clicked button as active
-        }
-    };
 
     return (
         <>
@@ -32,16 +24,7 @@ export function Room({socket}) {
             </ul>
 
             <h2>Vote:</h2>
-            {/* <Button variant="contained" color={isActive ? 'secondary' : 'primary'} onClick={handleClick}>3</Button> */}
-            <Button variant={activeButton === 0 ? 'contained' : 'outlined'} color={activeButton === 0 ? 'secondary' : 'primary'} onClick={() => handleButtonClick(0)} > 0 </Button>
-            <Button variant={activeButton === 0.5 ? 'contained' : 'outlined'} color={activeButton === 0.5 ? 'secondary' : 'primary'} onClick={() => handleButtonClick(0.5)} > 0.5 </Button>
-            <Button variant={activeButton === 1 ? 'contained' : 'outlined'} color={activeButton === 1 ? 'secondary' : 'primary'} onClick={() => handleButtonClick(1)} >1 </Button>
-            <Button variant={activeButton === 2 ? 'contained' : 'outlined'} color={activeButton === 2 ? 'secondary' : 'primary'} onClick={() => handleButtonClick(2)} >2 </Button>
-            <Button variant={activeButton === 3 ? 'contained' : 'outlined'} color={activeButton === 3 ? 'secondary' : 'primary'} onClick={() => handleButtonClick(3)} >3 </Button>
-            <Button variant={activeButton === 5 ? 'contained' : 'outlined'} color={activeButton === 5 ? 'secondary' : 'primary'} onClick={() => handleButtonClick(5)} >5 </Button>
-            <Button variant={activeButton === 8 ? 'contained' : 'outlined'} color={activeButton === 8 ? 'secondary' : 'primary'} onClick={() => handleButtonClick(8)} >8 </Button>
-            <Button variant={activeButton === 13 ? 'contained' : 'outlined'} color={activeButton === 13 ? 'secondary' : 'primary'} onClick={() => handleButtonClick(13)} >13 </Button>
-                    
+            <Points socket={socket}/>                    
         </>
     );
 }
